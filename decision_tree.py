2c60567e137a08d53124cacb47d6c16570a57a4d
seanprkk <seanprkk@gmail.com>
2024-09-17 17:24:22 -0700

    added csv file and reads

--- decision_tree.py
+++ decision_tree.py
@@ -1,1 +1,12 @@
-decisiontree.py
+import os
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv("data/riceClassiciation.csv"))
+X = df.drop(columns=['Class'])
+y = df['Class'].values
+
